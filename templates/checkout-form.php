e00288d42f400c76845767f42217fe31de82f874
75nineteen <75nineteen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 01:54:01 +0000

    initial release
    git-svn-id: https://plugins.svn.wordpress.org/twitter-follow-ups/trunk@1098794 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/checkout-form.php
@@ -0,0 +1,4 @@
+<p class="form-row form-row-wide">
+    <label class="" for="twitter_handle"><?php _e('Twitter Handle', 'follow_up_emails'); ?></label>
+    @<input type="text" value="" placeholder="username" id="twitter_handle" name="twitter_handle" class="input-text" style="width:90%">
+</p>
